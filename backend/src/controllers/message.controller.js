6c465de4260935410b83922ac037d00a934ebe70
Rishav Chaudhary <rishavc957@gmail.com>
2025-02-18 18:58:05 +0545

    message model, message controller, message routing, is been updated, socket.io is left for real-time functionality with some additional function as well

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,74 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
 export const getUsersforSidebar = async(req,res) => {
-    
+    try {
+        const loggedInUserId = req.user._id;
+        // tells mongoose, find all the users but do not find the loggedInUserId $ne: means not equals:
+        const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password");
+        res.status(200).json(filteredUsers)
+    } catch (error) {
+        console.log("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const getMessages = async(req,res) => {
+    try {
+        const { id:userToChatId } = req.params // to grab the /:id that is written in url using req.params, and in destructuring part, we just renamed the /:id with userToChatId
+        const myId = req.user._id;
+
+        // find all the messages where either i am the sender or the other one is sender
+        // so the filter used in find is an object where $or is used with a list, 
+        // here the object inside the list defined in $or is find messages where senderId is senderId and receiverId is userToChatId, or senderId is userToChatId
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId}, 
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error"})
+    }
+}
+
+// when we send the message, it will either be text or could be an image
+export const sendMessage = async(req,res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        // now checking if sender is sending image or not
+        let imageUrl; // undefined at first
+        // then, if image is true, then
+        if (image) {
+            // Uploading the base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        // creating new message form Message model made with messageSchema
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        })
+
+        // now saving the newMessage in database using mongoose
+        await newMessage.save();
+
+        // todo: realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+        
+    }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,18 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersforSidebar } from "../controllers/message.controller.js";
+import { getUsersforSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-// now we need to add endpoints or routes
+// now we need to add endpoints or routes, like all the users in sidebar except the one that is logged in
 router.get("/users", protectRoute, getUsersforSidebar)
+
+// upon clicking the sidebars profile, a new window beside scroll bar pops up, but that is ui. we will not work on that right now
+// but we do need the conversation history from first conversation to last coversation in chronological order of the one we clicked and us
+router.get("/:id",protectRoute,getMessages)
+
+// now to send the messages
+router.post("/send/:id", protectRoute, sendMessage)
 
 
 export default router;
